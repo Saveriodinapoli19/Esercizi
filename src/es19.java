605d171657f33d0828dd402019cab7e7b3f65da2
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-10-27 18:51:43 +0200

    esercizio 18

---
+++ src/es19.java
@@ -0,0 +1,23 @@
+//Scrivere un programma che contenga un metodo
+// che sfrutti il while loop e prenda in ingresso
+// 1 valore intero come limite superiore e
+// calcoli la somma di tutti i valori prima del limite e la stampi a video.
+//(Esempio passo 5 come limite e otterrò la somma di 1 + 2 + 3 + 4)
+
+ public class es19 {
+     public static void main(String[] args) {
+         int x = 1;
+
+     System.out.println(metodo(x));
+     }
+public static int metodo(int x){
+int sum= 1;
+         while(x<5){
+    System.out.println(x);
+x++;
+sum +=x;
+}
+return sum;
+     }
+
+}
