0b2d2118ab9bc9669641669100972fed58371a0a
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-10-27 15:50:05 +0200

    esercizio 16

---
+++ src/Es16.java
@@ -0,0 +1,18 @@
+//Scrivi un programma che calcoli la lunghezza di una stringa, sulla
+// base di questa lunghezza stampi
+// "Lunghezza maggiore di 10", "Lunghezza minore di 10" o "Lunghezza pari a 10".
+public class Es16{
+    public static void main(String[] args){
+        String name= "Luca";
+        int name1= name.length();
+
+        if(name1>10){
+            System.out.println("Il numero è maggiore 10");
+        } else if(name1==10){
+            System.out.println("Il numero è uguale 10");
+        }else{
+            System.out.println("Il numero è minore 10");
+        }
+
+    }
+}
