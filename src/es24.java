7724dcbecdb35fd391e5cf0ca5b9bb244871e2b4
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-10-31 12:20:52 +0100

    esercizio 24

--- src/es24.java
+++ src/es24.java
@@ -1,20 +1,24 @@
 //Scrivi un programma che contenga un metodo che crea un array e
 // lo riempie con i numeri da 1 a 10 e che che calcoli la somma dei
 // numeri contenuti nell'array inizializzato e la stampi a video.
-public class es24 {
-    public static void main(String[] args){
-int[] numeri = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-sum(numeri);
-    }
-public static int sum(int[] numeri){
-        int somma= 0;
- for(int i=0; i<= numeri.length; i++){
-     somma += numeri[i];
 
-     System.out.println(somma);
- }
-return somma;
+public class es24 {
+    public static void main (String[] args) {
+            int[] numeri = Array();
+            System.out.println("The sum of the elements of the array is: " + sum(numeri));
+        }
+        public static int[] Array() {
+            int[] numeri = new int[]{1,2,3,4,5,6,7,8,9,10};
+            for (int i = 0; i < numeri.length; i ++) {
+                numeri[i] = i + 1;
+            }
+            return numeri;
+        }
+        public static int sum(int[] numeri) {
+            int sum = 0;
+            for (int num1 : numeri) {
+                sum += num1;
+            }
+            return sum;
+        }
     }
-}
-//N.B so che l'esercizio può essere sbagliato perchè mi somma tutti gli elementi dell array, vorrei sapere se il procedimento
-//è giusto e come faccio ad avere la somma singola tra tutti i numeri
