ad0d993fd49ef0282f02f0d082e693aa7b8e9695
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-11-03 12:14:39 +0100

    moltiplicazione team3

---
+++ src/es1gruppo3.java
@@ -0,0 +1,9 @@
+public class es1gruppo3 {
+    public static void main(String[] args){
+        int x=5;
+        int y=10;
+        int z= x*y;
+        System.out.println("Il valore risultante è:" + " " + z);
+    }
+}
+
