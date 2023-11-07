d196d20320aff03b78f322c5190b443023149af3
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-11-07 12:16:48 +0100

    Multiply

---
+++ src/gruppo/Multiply.java
@@ -0,0 +1,62 @@
+package gruppo;
+
+public class Multiply {
+
+    private int x;
+    private int y;
+    private char calc;
+
+    public Multiply(int x, int y, char calc) {
+        this.x = x;
+        this.y = y;
+        this.calc = calc;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int newX) {
+        this.x = newX;
+    }
+
+    public int getY () {
+        return x;
+    }
+    public void setY (int newY) {
+        this.y = newY;
+    }
+
+    public char getCalc () {
+        return calc;
+    }
+    public void setCalc (char newCalc) {
+        this.calc = newCalc;
+    }
+
+    public int risolviOperazione() {
+        int result = 0;
+        switch (calc) {
+            case '+':
+                result = x + y;
+                break;
+            case '-':
+                result = x - y;
+                break;
+            case '*':
+                result = x * y;
+                break;
+            case '/':
+                if (y != 0) {
+                    result = x / y;
+                } else {
+                    System.out.println("Impossible to divide by zero.");
+                }
+                break;
+            case '^':
+                result = x ^ y;
+                break;
+            default:
+        }
+        return result;
+    }
+}
