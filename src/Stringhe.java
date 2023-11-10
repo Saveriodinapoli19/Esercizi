4110b86dfe78fd2e7c9d22f6f6a26d507030144d
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-11-10 11:48:22 +0100

    esercizio 38

--- src/Stringhe.java
+++ src/Stringhe.java
@@ -6,16 +6,26 @@
 //Per la creazione della stringa di risultato dovrebbe essere utilizzato StringBuilder.
 public class Stringhe {
     public static void main(String[] args) {
-        StringBuilder frase1= new StringBuilder("Oggi è domenica");
-        StringBuilder frase2= new StringBuilder ("Domani è domenica");
+            String Frase1 = "Hello";
+            String Frase2 = "World";
 
+            String risultatoConfronto = confrontaStringhe(Frase1, Frase2);
 
-        if(frase1.equals(frase2)){
-            System.out.println("Le due stringhe sono uguali");
-        }else{
-            System.out.println("Le due stringhe sono differenti");
+            System.out.println(risultatoConfronto);
+        }
+
+        public static String confrontaStringhe(String stringa1, String stringa2) {
+            int confronto = stringa1.compareTo(stringa2);
+            StringBuilder risultato = new StringBuilder("Le due stringhe sono ");
+
+            if (confronto == 0) {
+                risultato.append("uguali");
+            } else {
+                risultato.append("differenti");
+            }
+
+            return risultato.toString();
         }
     }
 
 
-}
